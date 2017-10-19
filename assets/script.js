5663020006ac6e84d1cbd4be1b1dc352a5623d16
seb26 <931319+seb26@users.noreply.github.com>
2017-10-19 10:11:07 -0500

    rejig the subcat nav entirely

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,58 @@
 $(document).ready(function() {
-  
+
   /* For: ALL */
-  
-  // Allow non-JS and JS styling
-  $("body").removeClass("no-js");
-  $("body").addClass("js");
-  
+
   // Use Unveil to dynamically load images with this class
   // $(".load-dyn").unveil(200);
-  
-  
+
+
+
+  // Nominate the element to act as the dynamic header
+  var navTarget = '.sc';
+  // Nomiante the invisible element which
+  // will allow the dynamic header to return when
+  // the user hovers over the bottom end of the page.
+  var navTargetHover = '.sc-hover';
+  // Hide Header on on scroll down
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 5;
+  var navbarHeight = $(navTarget).outerHeight();
+  $(window).scroll(function(event){
+      didScroll = true;
+  });
+  setInterval(function() {
+      if (didScroll) {
+          hasScrolled();
+          didScroll = false;
+      }
+  }, 250);
+  function hasScrolled() {
+      var st = $(this).scrollTop();
+      // Make sure they scroll more than delta
+      if(Math.abs(lastScrollTop - st) <= delta)
+          return;
+      // If they scrolled down and are past the navbar, add class .nav-up.
+      // This is necessary so you never see what is "behind" the navbar.
+      if (st > lastScrollTop && st > navbarHeight){
+          // Scroll Down
+          $(navTarget).removeClass('sc-down').addClass('sc-up');
+      } else {
+          // Scroll Up
+          if(st + $(window).height() < $(document).height()) {
+              $(navTarget).removeClass('sc-up').addClass('sc-down');
+          }
+      }
+      lastScrollTop = st;
+  };
+  $(navTargetHover).hover(function() {
+    $(navTarget).removeClass('sc-up').addClass('sc-down');
+  });
+
+
   /* For: index.html
    * Make the image info panel toggleable. */
-  
+
   $('.ii-icon').mousedown(function(e){ e.preventDefault(); }); // Prevent double-click selecting on the icon box
   var image_info_overlay = false;
   $('.ii-icon').click( function() {
@@ -27,8 +67,8 @@ $(document).ready(function() {
       clicked = $(e.target);
       parents = clicked.parents();
       if ( clicked.hasClass('ii-icon')
-          ||  parents.hasClass('ii') 
-          || parents.hasClass('ii-icon') 
+          ||  parents.hasClass('ii')
+          || parents.hasClass('ii-icon')
       ) {
         // If you click the icon, or the infobox
         return;
@@ -44,73 +84,6 @@ $(document).ready(function() {
       return;
     }
   });
-  
-  
-  $('.subcat-wrapper').mousedown(function(e){ e.preventDefault(); }); // Prevent double-click selecting on the icon box
-  var subcat_list = false;
-  $('#subcat-toggle-button').click( function() {
-    subcat_list = $(this).next();
-    $(subcat_list).toggle();
-  });
-  $(window).click( function(e) {
-    if ( subcat_list ) {
-      // User has clicked on the subcat dropdown
-      clicked = $(e.target);
-      parents = clicked.parents();
-      if ( clicked.hasClass('subcat-toggle') || 
-           clicked.hasClass('subcat-toggle-button-label' ) ) {
-        // If you click the icon, or the infobox
-        // Do nothing
-        return;
-      }
-      else {
-        // If you click anywhere else
-        $(subcat_list).hide();
-      }
-    }
-    else {
-      // User has not clicked on an Info bubble
-      return;
-    }
-  });
-     
-  
-  
-  /* For: index.html
-   * Make the Subcategory switcher change
-   * according to what section we are on. */
-  
-  /* DISABLED 
-  var page_segments = [ 'people', 'photojournalism', 'environment', 'urban', 'architecture' ];
-  $(window).scroll(function() {
-    page_segments.forEach(function(segment) {
-      var scroll = $(window).scrollTop();
-      var theSegment = $('#' + segment);
-      var os = theSegment.offset().top;
-      var ht = theSegment.prev().prev().height();
-      if( scroll > os - ht ) {
-        // Change the label
-        $('#subcat-toggle-button').text(theSegment.text());
-        // Remove the active class
-        $('.subcat-menu a').removeClass('item-active');
-        // Add the class again to just one item
-        $('#subcat-item-' + segment).addClass('item-active');
-      }
-    });
-  });
-  $('.subcat-menu a').click(function(e) {
-    $(e.target).removeClass('item-active');
-    $(e.target).parent().hide();
-    clicked_subcat = $(e.target).data('subcat');
-    // $('#subcat-menu-' + clicked_subcat).addClass('item-active');
-  });
-  // When user jumps to top, manually change
-  // the label so it says 'People'.
-  // I.e., the first category.
-  $('#subcat-jump-to-top').click(function(e) {
-    $('#subcat-toggle-button').text('People');
-  });
-  */
 
-    
+
 });
