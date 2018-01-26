1836bbe3f578ae69fc41436819c05ec3e1712d23
seb26 <931319+seb26@users.noreply.github.com>
2018-01-26 20:15:30 +1100

    Revert "Merge branch 'master' of https://github.com/seb26/seb26.github.io"
    This reverts commit 8965e85f6f857eb8ec3b5fd48e8b739bff91a272, reversing
    changes made to 494c426fc8bd9dcf851af60fd6a32e2bef985748.

--- js/script.js
+++ js/script.js
@@ -18,13 +18,7 @@ $( document ).ready( function() {
   //
 
   // (1) Lazy Loading
-  $('.lazy').show().Lazy({
-    afterLoad: function(e) {
-      // When done, remove this class, which gives the <img> the proper shape
-      // prior to being loaded
-      // e.parent().removeClass('notloaded');
-    }
-  });
+  $('.lazy').show().Lazy();
 
   // (2) Subcategory Navigator
   //
