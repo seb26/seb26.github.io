b3c59c67dfbb1fa2fc193da108bc5221a2a36f4b
Sebastian Reategui <seb.reategui@gmail.com>
2019-11-10 21:59:45 +1100

    - revamp video entirely (W.I.P.) - design is broken, so that needs attention

--- js/script.js
+++ js/script.js
@@ -134,4 +134,31 @@ $( document ).ready( function() {
     $(this).ekkoLightbox();
   });
 
+  // (7)
+  // VIDEO functionality
+  //
+      // Expand or contract deeper project info
+      $('.project-thumb').click( function(e) {
+        // Prevent normal click
+        // e.preventDefault();
+        var current_projectname = $( this ).data('projectname');
+
+        // Turn off all other projects
+        $('.infodeep').hide();
+        $('.project-thumb').removeClass('active');
+
+        // Switch on ours
+        $('#project-' + current_projectname).show();
+        $( this ).addClass('active');
+      });
+
+      // Add 'Back' button which contracts the info
+      // Without necessarily showing another project
+      $('.infodeep-close').click( function(e) {
+        e.preventDefault();
+        // Turn off all other projects
+        $('.infodeep').hide();
+        $('.project-thumb').removeClass('active');
+      });
+
 } );
